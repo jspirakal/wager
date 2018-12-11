af72b20c39477f73404c7169c0414c6e8acffcca
Cal <cal.wagergames@tutanota.com>
2018-12-11 17:12:47 +0800

    new update 4

--- src/components/Portfolio/SidebarLeft.vue
+++ src/components/Portfolio/SidebarLeft.vue
@@ -1,32 +1,322 @@
 <template>
+
     <div class="col-md-2 p-0">
-            <div class="sidebar-left-page3">
-                <ul>
-                        <li><a href="#"></a>Home<hr></li>
-                        <li><a href="#"></a>Team History<hr></li>
-                        <li><a href="#"></a>ScreenShot<hr></li>
-                        <li><a href="#"></a>Stream<hr></li>
-                        <li><a href="#"></a>Stats<hr></li>
-                        <li><a href="#"></a>Schedule<hr></li>
-                        <li><a href="#"></a>Managers<hr></li>
-                        <li><a href="#"></a>Interviews<hr></li>
-                        <li><a href="#"></a>Videos<hr></li>
-                        <li><a href="#"></a>Gallery<hr></li>
-                        <li><a href="#"></a>Online / LAN <hr></li>
-                        <li><a href="#"></a>Matches<hr></li>
-                        <li><a href="#"></a>Tournaments<hr></li>
-                        <li><a href="#"></a>Connect</li>
-                </ul>
-            </div>
-    </div>
+      <div class="sidebar-left-page3">
+        <ul>
+                
+          
+                  <li>
+            <li>
+                    <a class="" href="http://localhost:8080" style="text-decoration: none;">Home</a> 
+            <hr></li>
+            <li><a class="" href="#" style="text-decoration: none;">Team History</a><hr></li>
+          <div class="active" id="line1"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Screenshot</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+             <div class="active" id="line2"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Stream</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+            <div class="active" id="line3"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Stats</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+              <div class="active" id="line4"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Schedule</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+              <div class="active" id="line5"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Managers</a> 
+            <a class="" href="#" id="b" style="text-decoration: nonee;">Coming Soon</a>
+            <hr></li></div>
+              <div class="active" id="line6"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Interviews</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+            <div class="active" id="line7"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Stream</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+            <div class="active" id="line8"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Gallery</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+            <div class="active" id="line9"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Online/LAN</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+            <div class="active" id="line10"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Matches</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+<div class="active" id="line11"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Tournament</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+            <div class="active" id="line12"> 
+             <li><a class="" href="#" id="a" style="text-decoration: none;">Connect</a> 
+            <a class="" href="#" id="b" style="text-decoration: none;">Coming Soon</a>
+            <hr></li></div>
+
+           
+          
+         
+           
+         
+
+        </ul>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
+    data:function(){
+    },
     name: 'SidebarLeft'
 }
 </script>
 
 <style>
+div#line1 a#a {
+  display: inline;
+}
 
+div#line1:hover a#a {
+  display: none;
+  
+}
+
+div#line1 a#b {
+  display: none;
+}
+
+div#line1:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line2 a#a {
+  display: inline;
+}
+
+div#line2:hover a#a {
+  display: none;
+  
+}
+
+div#line2 a#b {
+  display: none;
+}
+
+div#line2:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line3 a#a {
+  display: inline;
+}
+
+div#line3:hover a#a {
+  display: none;
+  
+}
+
+div#line3 a#b {
+  display: none;
+}
+
+div#line3:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line4 a#a {
+  display: inline;
+}
+
+div#line4:hover a#a {
+  display: none;
+  
+}
+
+div#line4 a#b {
+  display: none;
+}
+
+div#line4:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line3 a#a {
+  display: inline;
+}
+
+div#line3:hover a#a {
+  display: none;
+  
+}
+
+div#line3 a#b {
+  display: none;
+}
+
+div#line3:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line4 a#a {
+  display: inline;
+}
+
+div#line4:hover a#a {
+  display: none;
+  
+}
+
+div#line4 a#b {
+  display: none;
+}
+
+div#line4:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line5 a#a {
+  display: inline;
+}
+
+div#line5:hover a#a {
+  display: none;
+  
+}
+
+div#line5 a#b {
+  display: none;
+}
+
+div#line5:hover a#b {
+  display: inline;
+    color: yellow
+}
+
+div#line6 a#a {
+  display: inline;
+}
+
+div#line6:hover a#a {
+  display: none;
+  
+}
+
+div#line6 a#b {
+  display: none;
+}
+
+div#line6:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line7 a#a {
+  display: inline;
+}
+
+div#line7:hover a#a {
+  display: none;
+  
+}
+
+div#line7 a#b {
+  display: none;
+}
+
+div#line7:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line8 a#a {
+  display: inline;
+}
+
+div#line8:hover a#a {
+  display: none;
+  
+}
+
+div#line8 a#b {
+  display: none;
+}
+
+div#line8:hover a#b {
+  display: inline;
+    color: yellow
+}
+
+div#line9 a#a {
+  display: inline;
+}
+
+div#line9:hover a#a {
+  display: none;
+  
+}
+
+div#line9 a#b {
+  display: none;
+}
+
+div#line9:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line10 a#a {
+  display: inline;
+}
+
+div#line10:hover a#a {
+  display: none;
+  
+}
+
+div#line10 a#b {
+  display: none;
+}
+
+div#line10:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line11 a#a {
+  display: inline;
+}
+
+div#line11:hover a#a {
+  display: none;
+  
+}
+
+div#line11 a#b {
+  display: none;
+}
+
+div#line11:hover a#b {
+  display: inline;
+    color: yellow
+}
+div#line12 a#a {
+  display: inline;
+}
+
+div#line12:hover a#a {
+  display: none;
+  
+}
+
+div#line12 a#b {
+  display: none;
+}
+
+div#line12:hover a#b {
+  display: inline;
+    color: yellow
+}
 </style>
