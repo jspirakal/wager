3c78fe03c66f461ad632704b351bdb66f17f1371
Cal <cal.wagergames@tutanota.com>
2018-12-11 16:56:51 +0800

    footer updated

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,5 +1,5 @@
 <style>
-#footer2 {
+#current-pane {
     position: absolute;
     z-index: 101;
 
@@ -74,74 +74,25 @@
                         <div class="col-md-12 mt-5 mb-5">
                                 <p class="copyright">Copyright © Wager Games. All rights reserved</p>
                         </div>
-                        <div class="footer2" id="footer2">
-
-<div id="myDIV">
-  <p>WagerGames uses tracking cookies to enable the best experience for you &nbsp; &nbsp;<b-button onclick="">I agree</b-button></p>
-  </div>
+                        <div class="footer2" id="current-pane">
+    <table class="table table-hover">
+     
+                Wager Games uses tracking cookies to optimize your web experience
+                <b-button href="#current-pane" class="close" data-dismiss="alert" aria-label="close" id="hide">I agree&times;</b-button>
+       
+  
+        <tbody>
+        </tbody>
+    </table>
 </div>
-     </footer>
+    </footer>
+</div>
+ 
 
 
 
 
 
-     <b-modal id="modal1" title="Media Submission Form">
-    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-      <b-form-group id="exampleInputGroup1"
-                    label="Email address:"
-                    label-for="exampleInput1"
-                    description="We'll never share your email with anyone else.">
-        <b-form-input id="exampleInput1"
-                      type="email"
-                      v-model="form.email"
-                      required
-                      placeholder="Enter email">
-        </b-form-input>
-      </b-form-group>
-      <b-form-group id="exampleInputGroup2"
-                    label="Your Name:"
-                    label-for="exampleInput2">
-        <b-form-input id="exampleInput2"
-                      type="text"
-                      v-model="form.name"
-                      required
-                      placeholder="Enter name">
-        </b-form-input>
-      </b-form-group>
-      <b-form-group id="exampleInputGroup3"
-                    label="User type:"
-                    label-for="exampleInput3">
-        <b-form-select id="exampleInput3"
-                      :options="foods"
-                      required
-                      v-model="form.food">
-        </b-form-select>
-      </b-form-group>
-      <b-form-group id="exampleGroup4">
-        <b-form-checkbox-group v-model="form.checked" id="exampleChecks">
-          <b-form-checkbox value="me">I agree to pay 1 WGT to use this service</b-form-checkbox>
-          <b-form-checkbox value="that">I agree to all terms and conditions*</b-form-checkbox>
-        </b-form-checkbox-group>
-      </b-form-group>
-       <b-form-group id="exampleInputGroup5"
-                    label="Your Story"
-                    label-for="exampleInput5">
-        <b-form-textarea id="exampleInput5"
-                      type="text"
-                      v-model="form.submission"
-                      required
-                      placeholder="Enter submission"
-                      rows="6">
-        </b-form-textarea>
-          <b-form-file v-model="file" accept="image/*" :state="Boolean(file)" placeholder="Choose a file..."></b-form-file>
-  <div class="mt-3">Selected file: {{file && file.name}}</div>
-         </b-form-group>
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
-
-  </b-modal> 
 
     </div>
 </template>
@@ -154,16 +105,6 @@ export default {
     }
   },
   name: 'MainFooter',
-  methods: {
-    Hide: function(){
-      var x = document.getElementById("myDIV");
-      if (x.style.display === "none") {
-          x.style.display = "block";
-      } else {
-          x.style.display = "none";
-      }
-    }
-  }
 }
 
 // var cookiesAlert = new vue({
