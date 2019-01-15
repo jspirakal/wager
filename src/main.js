7f81a7bafa00b84a6d2e1c5b73eda8abdf0098f5
salmansabir <salman.sabir2012@gmail.com>
2019-01-15 13:24:23 +0500

    App global configuration updated

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
+import VueSweetalert2 from 'vue-sweetalert2';
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = false
+
+// BOOTSTRAP
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Sweet Alert
+Vue.use(VueSweetalert2);
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
